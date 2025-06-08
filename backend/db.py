243db6b0ed86586d7f500a7d8f895b03d04b6de1
Ongun Güvenç <90259478+OngunGuvenc@users.noreply.github.com>
2025-06-08 20:47:45 +0300

    backend updated
    mysql connector

---
+++ backend/db.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",  # <-- db password
+    database="" # <-- db name
+)
+cursor = conn.cursor(dictionary=True)
